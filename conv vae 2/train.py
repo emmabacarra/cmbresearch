55a50c79f3b9210ce5a657086eb51277e4f42636
emmabacarra <ebacarra@uw.edu>
2024-07-16 21:12:41 -0700

    increased epochs

--- conv vae 2/train.py
+++ conv vae 2/train.py
@@ -93,7 +93,7 @@ nnet = net(model, train_loader, val_loader, test_loader, batch_size, linear=Fals
 optimizer = Adam(model.parameters(), lr=0.001, weight_decay=1e-10);
 
 if __name__ == '__main__':
-    nnet.train(optimizer=optimizer, lsfn=loss_function, epochs=15, kl_weight=0.1, live_plot=False, outliers=False)
+    nnet.train(optimizer=optimizer, lsfn=loss_function, epochs=30, kl_weight=0.1, live_plot=False, outliers=False)
     torch.save(nnet.model.state_dict(), 'saved_model.pth')
     nnet.evaluate(test_loader)
 
